74069190cc2e271148cac09c6833b2ed00ec6f84
jymfignt <147328457+jymfignt@users.noreply.github.com>
2025-06-12 20:01:14 +0800

    Update Transport.h

--- Transport.h
+++ Transport.h
@@ -13,14 +13,15 @@ protected:
     int maxPeople;
     int maxWeight;
     string category;
+    bool available=true;
 public:
 
     Transport(string id_, string name_, string category_, int p, int w)
         : id(id_), name(name_), category(category_), maxPeople(p), maxWeight(w) {}
     Transport()=default;
     virtual ~Transport() = default;
-
-
+    virtual bool isAvailable() const { return available; }
+    virtual void setAvailable(bool status) { available = status; }
     void setID(string i){id=i;}
     void setName(string n){name=n;}
     void setCategory(string c)  { category=c; }
@@ -39,7 +40,8 @@ public:
     virtual double getSpeed() const = 0;
     virtual string serialize() const {
     return id + "," + name + "," + category + "," +
-           to_string(maxPeople) + "," + to_string(maxWeight);
+           to_string(maxPeople) + "," + to_string(maxWeight)
+            +"," + (available?"1":"0");
 }
     virtual void displayInfo() const {
         cout << "ID: " << id
